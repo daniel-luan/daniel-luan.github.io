f40447182ec1c2b0d23ff77711896853cdb43b98
Daniel Luan <DanielLuan425@gmail.com>
2019-08-20 14:13:24 -0400

    Updates

---
+++ precache-manifest.fe5263bfd05e2eb8be8be5663270f83c.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c3613c84dc0beec87c030c114cfbdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab3d38e19ae39d99b9f",
+    "url": "/static/css/main.47a48738.chunk.css"
+  },
+  {
+    "revision": "4aab23631c301bf7f08d",
+    "url": "/static/js/2.e8372b60.chunk.js"
+  },
+  {
+    "revision": "2ab3d38e19ae39d99b9f",
+    "url": "/static/js/main.d29f77b0.chunk.js"
+  },
+  {
+    "revision": "bfd14db7b975b9377460",
+    "url": "/static/js/runtime~main.b171f8b8.js"
+  },
+  {
+    "revision": "b5cf9edccc37b9558676f721d1c2e14d",
+    "url": "/static/media/Android_greenrobot.b5cf9edc.svg"
+  },
+  {
+    "revision": "c171886490e1f14aee76b84e8246d6d3",
+    "url": "/static/media/Arduino_Logo.c1718864.svg"
+  },
+  {
+    "revision": "b9ee8fb6de937f707ae1d0128e8a6015",
+    "url": "/static/media/Bash_Logo_Favicon_Black_and_White.b9ee8fb6.svg"
+  },
+  {
+    "revision": "06596577ae4143d9d51e2184310e23dd",
+    "url": "/static/media/Boostrap_logo.06596577.svg"
+  },
+  {
+    "revision": "d453faf94bb49cb16b6fa4ae54087df2",
+    "url": "/static/media/Django.d453faf9.svg"
+  },
+  {
+    "revision": "aaec9219d68861e16f79211b86d4df6d",
+    "url": "/static/media/Git-logo.aaec9219.svg"
+  },
+  {
+    "revision": "3c81698cf9342ca6d72f93b2a7faf87c",
+    "url": "/static/media/Go_Logo_Aqua.3c81698c.svg"
+  },
+  {
+    "revision": "8cc47c68f0d71b364d6e3b793dbb6e55",
+    "url": "/static/media/ISO_C++_Logo.8cc47c68.svg"
+  },
+  {
+    "revision": "cdf5b5e1a8dd51c1130b4d4a86048b8a",
+    "url": "/static/media/PHP-logo.cdf5b5e1.svg"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/static/media/Python-logo-notext.fbef321e.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/React-icon.05a3e30f.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/Unofficial_JavaScript_logo_2.dd60b11e.svg"
+  },
+  {
+    "revision": "5c69e565f1ae6d7717fb3ecd2be004ad",
+    "url": "/static/media/angular.5c69e565.svg"
+  },
+  {
+    "revision": "2e0fcd7b793e8353f3e272dc38492020",
+    "url": "/static/media/docker.2e0fcd7b.svg"
+  },
+  {
+    "revision": "318d58cbc47ace3e1baf4f035cab5b53",
+    "url": "/static/media/flask-seeklogo.com.318d58cb.svg"
+  },
+  {
+    "revision": "a732a6e5bc69653a55d6dd81ab8d2d5e",
+    "url": "/static/media/java-seeklogo.com.a732a6e5.svg"
+  },
+  {
+    "revision": "efa8fbd95ccccfe17a7eb301c28245ae",
+    "url": "/static/media/kubernetes-seeklogo.com.efa8fbd9.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "1c28eea7134ce74666d34852b8713d0d",
+    "url": "/static/media/nginx.1c28eea7.svg"
+  },
+  {
+    "revision": "7d9fccaffda0db8340fe3e803fee9c83",
+    "url": "/static/media/python-logo-generic.7d9fccaf.svg"
+  }
+]);
